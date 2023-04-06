d515771c9f96c6b51e94792043eb2373d04b451c
liyifang <leergo@dingtalk.com>
2023-04-06 09:45:30 +0800

    fea: 更新 maven 脚本
    Signed-off-by: liyifang <leergo@dingtalk.com>

--- app/src/androidTest/java/com/arpa/and/table/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/arpa/and/table/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.arpa.and.appupdate;
+package com.arpa.and.table;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.arpa.and.appupdate", appContext.getPackageName());
+        assertEquals("com.arpa.and.table", appContext.getPackageName());
     }
 }
